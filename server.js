5b62c19403d1196506e88a63bcb66661deaa3b92
jordan <jordanvang8@gmail.com>
2020-12-03 11:48:46 -0600

    database update

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
